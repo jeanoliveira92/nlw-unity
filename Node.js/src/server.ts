5fbeddb7c58f649c4f3528ebdf309c2c2ae4aa1a
Jean Carlos de Oliveira <jeancarlosdeoliveia@outlook.com>
2024-04-03 16:54:15 -0300

    Aula 2 - Node.js

--- Node.js/src/server.ts
+++ Node.js/src/server.ts
@@ -1,35 +1,20 @@
 import fastify from "fastify";
-import { z } from "zod";
-import { PrismaClient } from "@prisma/client"
+
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod"
+import { createEvent } from "./routes/create-event";
+import { registerForEvent } from "./routes/register-for-events";
+import { getEvent } from "./routes/get-event";
+import { getAttendeeBagde } from "./routes/get-attendee-badge";
 
 const app = fastify()
 
-const prisma = new PrismaClient({
-    log: ['query']
-})
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
-app.post("/events", async (request, reply) => {
-
-    const createEventSchema = z.object({
-        "title": z.string().min(4),
-        "details": z.string().nullable(),
-        "maximumAttendees": z.number().int().positive().nullable()
-    })
-
-    const data = createEventSchema.parse(request.body)
-
-    const event = await prisma.event.create({
-        data: {
-            title: data.title,
-            details: data.details,
-            maximumAttendess: data.maximumAttendees,
-            slug: new Date().toISOString()
-        }
-    })
-
-    //return { eventId: event.id }
-    return reply.status(201).send({ eventId: event.id })
-})
+app.register(createEvent)
+app.register(registerForEvent)
+app.register(getEvent)
+app.register(getAttendeeBagde)
 
 app.listen({ port: 3333 }).then(() => {
     console.log("HTTP server running!")
